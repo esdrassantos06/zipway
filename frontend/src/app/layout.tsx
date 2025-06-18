be2bc72ecc2c1aa4f307cc62f72e1114e91d6f75
Esdras Santos <esdrasirion1@gmail.com>
2025-06-18 11:21:25 +0100

    Refactor Google Ads script integration in RootLayout component. This update replaces the Script component with Head for better semantic structure and consistency in the layout. The change maintains the existing ad functionality while improving the overall organization of the codebase, contributing to enhanced maintainability and scalability.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,11 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="overflow-hidden">
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4813194510722281"
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4813194510722281"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
       <body
         className={`${inter.className} antialiased`}
         suppressHydrationWarning
