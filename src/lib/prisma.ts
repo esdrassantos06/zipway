81ec09a9faa21f6de49f6e5474aeb512c0ec6d73
Esdras Santos <esdrasirion1@gmail.com>
2025-12-13 18:01:40 +0000

    Update dependencies, enhance Prisma configuration, and refactor database connection logic
    This commit updates various dependencies in `package.json` and `package-lock.json`, including improvements to libraries such as `@hookform/resolvers`, `@prisma/client`, and `react`. Additionally, a new `prisma.config.ts` file is introduced to streamline Prisma configuration, while the existing database connection logic in `prisma.ts` is refactored to utilize the new `@prisma/adapter-pg`. The `schema.prisma` file is also updated to remove the direct URL configuration, enhancing maintainability and clarity. These changes collectively improve the application's performance and scalability.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
